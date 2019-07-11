21325d354c2694a9b461b2b9dd2e9bbf343eb05c
Lantz Warrick <betalantz@gmail.com>
2019-07-11 23:01:54 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -18,7 +18,7 @@ class CashRegister
   end
 
   def apply_discount
-    if @discount != 0
+    if @discount
       # @total = @total.to_f * (1 - @discount.to_f / 100.0)
       self.total -= self.total * (self.discount/100.0)
       "After the discount, the total comes to $#{@total.to_i}."
